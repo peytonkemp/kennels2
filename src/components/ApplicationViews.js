3dd191d3f975423ea79e7cd64e105399f8fdfefd
peytonkemp <peyton.kemp@gmail.com>
2021-06-03 15:48:09 -0500

    added navbar

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { LocationProvider } from "./locations/LocationProvider"
+import { LocationList } from "./locations/LocationList"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <LocationProvider>
+                <Route exact path="/">
+                    <LocationList />
+                </Route>
+            </LocationProvider>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <AnimalProvider>
+                <Route path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
+        </>
+    )
+}
--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -9,10 +9,14 @@ import { AnimalList } from "./animal/AnimalList"
 // import { EmployeeList } from "./employee/EmployeeList"
 // import { LocationProvider } from "./location/LocationProvider"
 // import { LocationList } from "./location/LocationList"
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
 
 
 export const Kennel = () => (
     <>
+        <NavBar />
+        <ApplicationViews />
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
 
