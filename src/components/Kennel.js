9abfe8c5088cacf1c349f2a6015017c1424581f5
peytonkemp <peyton.kemp@gmail.com>
2021-06-10 15:29:34 -0500

    added auth

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,53 +1,33 @@
-import React from "react"
-import "./Kennel.css"
-import { AnimalProvider } from "./animals/AnimalProvider"
-import { AnimalList } from "./animals/AnimalList"
-import { CustomerProvider } from "./customers/CustomerProvider"
-import { CustomerList } from "./customers/CustomerList"
-import { EmployeeProvider } from "./employees/EmployeeProvider"
-import { EmployeeList } from "./employees/EmployeeList"
-// import { LocationProvider } from "./location/LocationProvider"
-// import { LocationList } from "./location/LocationList"
-import { NavBar } from "./nav/NavBar";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
-
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Kennel.css";
 
 export const Kennel = () => (
     <>
-        <NavBar />
-        <ApplicationViews />
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
+        <Route
+            render={() => {
+                if (localStorage.getItem("kennel_customer")) {
+                    return (
+                        <>
+                            <NavBar />
+                            <ApplicationViews />
+                        </>
+                    );
+                } else {
+                    return <Redirect to="/login" />;
+                }
+            }}
+        />
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
     </>
-)
-
-        // <h2>Animals</h2>
-        // <article className="animals">
-        //     <AnimalProvider>    
-        //         <AnimalList />
-        //     </AnimalProvider>
-        // </article>
-        // <article className="customers">
-        //     <CustomerProvider>
-        //         <CustomerList />
-        //     </CustomerProvider>
-        // </article>
-        // <article className="employees">
-        //     <EmployeeProvider>
-        //         <EmployeeList />
-        //     </EmployeeProvider>
-        // </article>
-
-            // <EmployeeProvider>
-            //     <EmployeeList />
-            // </EmployeeProvider>
-
-            // <LocationProvider>
-            //     <LocationList />
-            // </LocationProvider>
+);
